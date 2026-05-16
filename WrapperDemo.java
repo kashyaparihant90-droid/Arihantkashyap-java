d98faa9165d21d96b2239bd603b2870f51c4b21a
kashyaparihant90-droid <kashyaparihant90@gmail.com>
2026-05-16 17:20:47 +0530

    WrapperDemo.java

---
+++ WrapperDemo.java
@@ -0,0 +1,17 @@
+public class public class WrapperDemo {
+    public static void main(String[] args) {
+
+        Integer a = 10;
+        int b = a; // unboxing
+
+        System.out.println(b);
+    }
+} {
+    public static void main(String[] args) {
+
+        Integer a = 10;
+        int b = a; // unboxing
+
+        System.out.println(b);
+    }
+}
